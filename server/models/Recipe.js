44ed29f12c137ed723524b7ee1db26200ed4bca8
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-01-24 14:06:21 +0100

    new commit added

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -6,7 +6,30 @@ const recipeSchema = new mongoose.Schema({
         type: String,
         required:'This Field is required'
     },
-   
+    description:{
+        type: String,
+        required:'This Field is required'
+    },
+    email:{
+        type: String,
+        required:'This Field is required'
+    },
+    ingredients:{
+        type: Array,
+        required:'This Field is required'
+    },
+    category:{
+        type: String,
+        enum:['Thai','American', 'Chinese', 'Mexican','Indian'],
+        required:'This Field is required'
+    },
+    image:{
+        type: String,
+        required:'This Field is required'
+    },
+
+
+
 
 }) 
 
